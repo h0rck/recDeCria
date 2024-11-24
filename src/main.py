37d05d3abee816ad40b2c48394a70a9f56da6e59
Sam <s4muelrp@gmail.com>
2024-11-24 14:03:54 -0300

    Add main function to initialize and run the application with face detection and recognition

---
+++ src/main.py
@@ -0,0 +1,22 @@
+# tem que rodar isso export PYTHONPATH=$(pwd)/src:$PYTHONPATH2
+# import sys
+# import os
+# sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+from infrastructure.recognition.opencv_detector import OpenCVFaceDetector
+from infrastructure.recognition.opencv_recognizer import OpenCVFaceRecognizer
+from presentation.cli.main_menu import MainMenu
+from presentation.camera.camera_manager import CameraManager
+from infrastructure.database.sqlite_event_storage import SQLiteEventStorage
+
+def main():
+    detector = OpenCVFaceDetector()
+    recognizer = OpenCVFaceRecognizer()
+    camera = CameraManager()
+    event_storage = SQLiteEventStorage()
+    
+    menu = MainMenu(detector, recognizer, camera, event_storage)
+    menu.run()
+
+if __name__ == "__main__":
+    main()
