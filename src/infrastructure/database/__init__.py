e36cf771add611a1eaf8d2a4cb67151e06327d5b
Sam <s4muelrp@gmail.com>
2024-11-24 04:40:43 -0300

    Refactor import path for DatabaseManager to improve consistency

--- src/infrastructure/database/__init__.py
+++ src/infrastructure/database/__init__.py
@@ -1,4 +1,4 @@
-from .database_manager import DatabaseManager
+from infrastructure.database.database_manager import DatabaseManager
 from infrastructure.database.sqlite_event_storage import SQLiteEventStorage
 
 __all__ = ['DatabaseManager', 'SQLiteEventStorage']
